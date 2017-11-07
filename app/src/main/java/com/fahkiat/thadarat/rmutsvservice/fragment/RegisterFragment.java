81c622cfa63aa71efa103e39972b61136287d11b
Thadarat <awayge.23@gmail.com>
2017-11-07 12:02:27 +0700

    before Lunch

---
+++ app/src/main/java/com/fahkiat/thadarat/rmutsvservice/fragment/RegisterFragment.java
@@ -0,0 +1,24 @@
+package com.fahkiat.thadarat.rmutsvservice.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.fahkiat.thadarat.rmutsvservice.R;
+
+/**
+ * Created by lenovo on 7/11/2560.
+ */
+
+public class RegisterFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_resister, container, false);
+        return view;
+    }
+}   // Main Class
