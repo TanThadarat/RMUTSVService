83170f8d0e34fc4b59405096f8c1124377170245
Thadarat <awayge.23@gmail.com>
2017-11-09 14:23:56 +0700

    Creatwe ListView Success

--- app/src/main/java/com/fahkiat/thadarat/rmutsvservice/fragment/ServiceFragment.java
+++ app/src/main/java/com/fahkiat/thadarat/rmutsvservice/fragment/ServiceFragment.java
@@ -8,9 +8,16 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.fahkiat.thadarat.rmutsvservice.MyServiceActivity;
 import com.fahkiat.thadarat.rmutsvservice.R;
+import com.fahkiat.thadarat.rmutsvservice.utility.GetAllData;
+import com.fahkiat.thadarat.rmutsvservice.utility.ListViewAdapter;
+import com.fahkiat.thadarat.rmutsvservice.utility.Myconstant;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 /**
  * Created by lenovo on 9/11/2560.
@@ -36,6 +43,45 @@ public class ServiceFragment extends android.support.v4.app.Fragment{
 //        Create Toolbar
 
         createToolbar(strings[1]);
+
+//        Create ListView
+        createListView();
+
+
+    }
+
+    private void createListView() {
+        ListView listView = getView().findViewById(R.id.livUser);
+        Myconstant myconstant = new Myconstant();
+        try {
+            GetAllData getAllData = new GetAllData(getActivity());
+            getAllData.execute(myconstant.getUrlGetAllUser());
+            String resultJSON = getAllData.get();
+            Log.d("9novV1", "JSON ==> " + resultJSON);
+            JSONArray jsonArray = new JSONArray(resultJSON);
+
+            String[] nameStrings = new String[jsonArray.length()];
+            String[] catStrings = new String[jsonArray.length()];
+            String[] userStrings = new String[jsonArray.length()];
+            String[] passwordStrings = new String[jsonArray.length()];
+
+            for (int i=0; i<jsonArray.length(); i+=1) {
+
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                nameStrings[i] = jsonObject.getString("Name");
+                catStrings[i] = jsonObject.getString("Category");
+                userStrings[i] = jsonObject.getString("User");
+                passwordStrings[i] = jsonObject.getString("Password");
+
+            }   // fpr
+                ListViewAdapter listViewAdapter = new ListViewAdapter(getActivity(),
+                        nameStrings, catStrings, userStrings, passwordStrings);
+                listView.setAdapter(listViewAdapter);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
     private void createToolbar(String setTile) {
